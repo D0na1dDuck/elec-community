debe5b1254b46258f3b54a1314e9d27cb6db993d
d0na1dduck <d0na1ddcuk6914@gmail.com>
2020-03-31 15:38:31 +0800

    修改配置文件

--- src/main/java/xyz/esp8266/community/controller/AuthorizeController.java
+++ src/main/java/xyz/esp8266/community/controller/AuthorizeController.java
@@ -1,6 +1,7 @@
 package xyz.esp8266.community.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,15 +14,21 @@ import xyz.esp8266.community.provider.GithubProvider;
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("23cea2678c592912ef5f");
-        accessTokenDTO.setClient_secret("4a8515f65cd83789be5954b8390e4f1102cc1cf5");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("https://elec.utools.club/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
