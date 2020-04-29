e8cbb3d7d35afc406b3c35551327ccc789e99d51
d0na1dduck <d0na1ddcuk6914@gmail.com>
2020-04-29 13:51:45 +0800

    完成问题详情页面

---
+++ src/main/java/xyz/esp8266/community/controller/QuestionController.java
@@ -0,0 +1,26 @@
+package xyz.esp8266.community.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import xyz.esp8266.community.dto.QuestionDTO;
+import xyz.esp8266.community.mapper.QuestionMapper;
+import xyz.esp8266.community.service.QuestionService;
+
+import javax.annotation.Generated;
+
+@Controller
+public class QuestionController {
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping("/question/{id}")
+    public String question(@PathVariable(name = "id") Integer id,
+                           Model model){
+        QuestionDTO questionDTO = questionService.getById(id);
+       model.addAttribute("question", questionDTO);
+        return "question";
+    }
+}
