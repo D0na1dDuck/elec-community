dfdc42444c65a4883c59ae492a8362190a60a96c
d0na1dduck <d0na1dduck6914@gmail.com>
2020-05-10 23:59:17 +0800

    问题页面相关问题的展示

--- src/main/java/xyz/esp8266/community/controller/QuestionController.java
+++ src/main/java/xyz/esp8266/community/controller/QuestionController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import xyz.esp8266.community.dto.CommentDTO;
 import xyz.esp8266.community.dto.QuestionDTO;
 import xyz.esp8266.community.enums.CommentTypeEnum;
+import xyz.esp8266.community.model.Question;
 import xyz.esp8266.community.service.CommentService;
 import xyz.esp8266.community.service.QuestionService;
 import java.util.List;
@@ -22,10 +23,12 @@ public class QuestionController {
     public String question(@PathVariable(name = "id") Long id,
                            Model model){
         QuestionDTO questionDTO = questionService.getById(id);
+        List<QuestionDTO> relatedQuestions = questionService.selectRelated(questionDTO);
         List<CommentDTO> comments = commentService.listByQuestionId(id, CommentTypeEnum.QUESTION);
         questionService.incView(id);
         model.addAttribute("question", questionDTO);
         model.addAttribute("comments", comments);
+        model.addAttribute("relatedQuestions", relatedQuestions);
         return "question";
     }
 }
--- src/main/java/xyz/esp8266/community/mapper/QuestionExtMapper.java
+++ src/main/java/xyz/esp8266/community/mapper/QuestionExtMapper.java
@@ -1,8 +1,10 @@
 package xyz.esp8266.community.mapper;
 
 import xyz.esp8266.community.model.Question;
+import java.util.List;
 
 public interface QuestionExtMapper {
     int incView(Question record);
     int incCommentCount(Question record);
+    List<Question> selectRelated(Question question);
 }
